edf9b3514aa81fd92991d1ed31b209a0ef4d1336
Janos Gyerik <janos@axiom>
2012-03-23 23:41:18 +0100

    added some input fields, aromas with multiautocompletetextview

--- src/com/mywinenotes/WineDetailsActivity.java
+++ src/com/mywinenotes/WineDetailsActivity.java
@@ -2,6 +2,8 @@ package com.mywinenotes;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.MultiAutoCompleteTextView;
 
 public class WineDetailsActivity extends Activity {
 
@@ -10,5 +12,28 @@ public class WineDetailsActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.wine_details);
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_dropdown_item_1line, AROMAS);
+		MultiAutoCompleteTextView textView = (MultiAutoCompleteTextView) findViewById(R.id.aromas);
+		textView.setAdapter(adapter);
+		textView.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
 	}
+
+	private static final String[] AROMAS = new String[] {
+		"Wow",
+		"Fruity",
+		"Woody",
+		"Mineral",
+		"Mushroomy",
+		"Acidic",
+		"Intense",
+		"Strong",
+		"Light",
+		"Heavy",
+		"Cherry",
+		"Vanilla",
+		"Sweet",
+		"Sour",
+	};
 }

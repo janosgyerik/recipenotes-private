fb7165026014ccc829133567bae3be157253d8c4
Janos Gyerik <janos@axiom>
2012-08-19 20:29:25 +0200

    delete recipe works

--- src/com/recipenotes/RecipeListActivity.java
+++ src/com/recipenotes/RecipeListActivity.java
@@ -6,10 +6,13 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.provider.BaseColumns;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.SimpleCursorAdapter;
@@ -19,6 +22,9 @@ public class RecipeListActivity extends ListActivity {
 
 	// Debugging
 	private static final String TAG = "RecipeListActivity";
+	
+	private SQLiteOpenHelper helper;
+	private Cursor cursor;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -26,20 +32,21 @@ public class RecipeListActivity extends ListActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.recipe_list);
 
-		SQLiteOpenHelper helper = new RecipeNotesSQLiteOpenHelper(this);
+		helper = new RecipeNotesSQLiteOpenHelper(this);
 		//helper.getWritableDatabase().execSQL("DELETE from main_recipe where name = ''");
-		Cursor mCursor = helper.getWritableDatabase().query(
+		
+		cursor = helper.getReadableDatabase().query(
 				"main_recipe", 
 				new String[]{ 
 						BaseColumns._ID, "name", "summary", "display_name",
 						}, 
 				null, null, null, null, "name");
-		startManagingCursor(mCursor);
+		startManagingCursor(cursor);
 
 		ListAdapter adapter = new SimpleCursorAdapter(
 				this, // Context.
 				R.layout.recipe_list_item,
-				mCursor,
+				cursor,
 				new String[] { 
 						BaseColumns._ID, "display_name",
 						},
@@ -50,6 +57,7 @@ public class RecipeListActivity extends ListActivity {
 		setListAdapter(adapter);
 		
 		getListView().setOnItemClickListener(new RecipeListItemClickListener());
+		getListView().setOnItemLongClickListener(new RecipeListItemLongClickListener());
 
 		((Button)findViewById(R.id.btn_add_recipe)).setOnClickListener(new AddRecipeOnClickListener());
 	}
@@ -67,14 +75,24 @@ public class RecipeListActivity extends ListActivity {
 	}
 	
 	class AddRecipeOnClickListener implements OnClickListener {
-
 		@Override
 		public void onClick(View v) {
 			Intent intent = new Intent(RecipeListActivity.this, RecipeDetailsActivity.class);
 			intent.putExtra("dummy", "dummy");
 			startActivity(intent);
 		}
-		
+	}
+	
+	class RecipeListItemLongClickListener implements OnItemLongClickListener {
+		@Override
+		public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+				int arg2, long arg3) {
+			String selectedItemId = String.valueOf(arg3);
+			Log.d(TAG, "selectedItemId = " + selectedItemId);
+			helper.getWritableDatabase().delete("main_recipe", "_id = ?", new String[]{ selectedItemId });
+			helper.getWritableDatabase().delete("main_recipeingredient", "recipe_id = ?", new String[]{ selectedItemId });
+			cursor.requery();
+			return true;
+		}
 	}
 }
-
